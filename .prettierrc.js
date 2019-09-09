4ad01643646e585e5281938079f9f90701d6d433
Adam <adam.ponting@gmail.com>
2019-09-09 16:36:35 +0100

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,6 @@
 module.exports = {
+  // This will convert double quotes to single quotes,
+  // and make sure that semicolons are not automatically inserted.
   singleQuote: true,
   semi: false
 }
-//This will convert double quotes to single quotes, and make sure that semicolons are not automatically inserted.
